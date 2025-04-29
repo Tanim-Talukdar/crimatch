04e175e7871a430f8dbadae5ad82317256a24f1a
Tanim-Talukdar <talukdertanim73@gmail.com>
2025-04-30 01:08:56 +0600

    Update Homepage.jsx

--- frontend/src/components/Home/Homepage.jsx
+++ frontend/src/components/Home/Homepage.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Hero from './Heroo'
+import Overview from './overview'
 
 
 export default function Homepage() {
   return (
     <>
       <Hero/>
-
+      <Overview/>
     </>
   )
 }
