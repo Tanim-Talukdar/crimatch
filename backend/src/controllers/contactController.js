d268232be913ace67d73c35727d0d4147b804aa4
Tanim-Talukdar <talukdertanim73@gmail.com>
2025-04-30 00:34:16 +0600

    Update contactController.js

--- backend/src/controllers/contactController.js
+++ backend/src/controllers/contactController.js
@@ -1,35 +1,31 @@
 import httpStatus from "http-status";
 import Contact from "../models/contactfrom.js";
+import { User } from "../models/user.js";
+
+
 
 
 const contactInfo = async (req,res) => {
-    try {
-
         const { email,subject , message} = req.body;
-    
-
         if (!email || !message) {
           return res.status(httpStatus.BAD_REQUEST).json({ message: "Missing required fields" });
         }
-    
-        const newItem = new Listing({
+        const userId = req.user.id;
+        const user = await User.findById(userId);
+          if (!user) {
+            return res.status(httpStatus.NOT_FOUND).json({ message: "User not found" });
+          }
+        const newItem = new Contact({
           email,
-          subject,    // Changed from image to imageObj
+          subject,    
           message,
+          user : userId
         });
-    
         await newItem.save();
-    
         res.status(httpStatus.CREATED).json({
           message: "Listing created successfully",
-          listing: newItem,
         });
-      } catch (error) {
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-          message: "Failed to create listing",
-          error: error.message,
-        });
-      }
+      
     };
 
     export {contactInfo};
