b906df81a2a79a3b3ddd3b4f7b50c589eaec7e7f
Tanim-Talukdar <talukdertanim73@gmail.com>
2025-04-30 00:28:35 +0600

    Update middleware.js

--- backend/middleware.js
+++ backend/middleware.js
@@ -1,6 +1,9 @@
 import jwt from 'jsonwebtoken';
 import {User} from './src/models/user.js';
-const JWT_SECRET = 'your_jwt_secret'; // Store this in .env in real projects
+import dotenv from "dotenv";
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET; // Store this in .env in real projects
 
 
 
@@ -8,41 +11,21 @@ export const isLogin = async (req, res, next) => {
   const authHeader = req.headers['authorization'];
 
   // No header or no token → 401 “User not logged in”
-  if (!authHeader) 
-    return res.status(401).json({ message: "User not logged in" });
+  if (!authHeader) return res.status(401).json({ message: "User not logged inn" });
 
   const token = authHeader.split(' ')[1];
-  if (!token) 
-    return res.status(401).json({ message: "User not logged in" });
+  if (!token) return res.status(401).json({ message: "User not logged innn" });
 
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
-    const user = await User.findById(decoded.userId);
-    if (!user) 
-      return res.status(401).json({ message: "User not logged in" });
+    const user = await User.findById(decoded.id);
+    if (!user) return res.status(401).json({ message: "User not logged innnnn" });
 
     req.user = user;
+
     next();
   } catch (err) {
     // invalid or expired token → also 401 “User not logged in”
-    return res.status(401).json({ message: "User not logged in" });
+    return res.status(401).json({ message: "server Erron" });
   }
-};
-
-
-export const verifyJWT = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  if (!authHeader) return res.status(401).json({ message: "Authorization header missing" });
-
-  const token = authHeader.split(' ')[1];
-  if (!token) return res.status(403).json({ message: "Token missing" });
-
-  jwt.verify(token, JWT_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ message: "Invalid token" });
-
-    if (user.role !== 'admin') return res.status(403).json({ message: "Access denied" });
-
-    req.user = user;
-    next();
-  });
 };
