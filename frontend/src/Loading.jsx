2ba956a90877428151dacfa83a9902198b198088
Tanim-Talukdar <talukdertanim73@gmail.com>
2025-05-01 05:44:22 +0600

    Add files via upload

---
+++ frontend/src/Loading.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="loading-page">
+      <div className="loader">
+        <div className="spinner"></div>
+        <p>Loading...</p>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
