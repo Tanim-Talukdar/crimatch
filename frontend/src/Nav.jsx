7243c42c17940b46bfa23f34a9c55d83cf57c455
Tanim-Talukdar <talukdertanim73@gmail.com>
2025-04-30 21:09:54 +0600

    Update Nav.jsx

--- frontend/src/Nav.jsx
+++ frontend/src/Nav.jsx
@@ -1,26 +1,27 @@
-import React, { useContext,useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { AuthContext } from './context/authcontext';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import PersonIcon from '@mui/icons-material/Person';
-import { Link,useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function Nav() {
-  const { token, handleLogout, userData } = useContext(AuthContext); // Added userData to access admin status
+  const { token, handleLogout, userData } = useContext(AuthContext);
   const location = useLocation();
 
-useEffect(() => {
-  const collapse = document.getElementById('navbarSupportedContent');
-  const bsCollapse = new window.bootstrap.Collapse(collapse, {
-    toggle: false,
-  });
-  bsCollapse.hide(); // hide on route change
-}, [location]);
+  useEffect(() => {
+    const collapse = document.getElementById('navbarSupportedContent');
+    const bsCollapse = new window.bootstrap.Collapse(collapse, {
+      toggle: false,
+    });
+    bsCollapse.hide();
+  }, [location]);
+
   return (
     <>
       {/* TOP NAVBAR */}
-      <nav className="navbar navbar-expand-lg bg-white border-bottom">
+      <nav className="navbar navbar-expand-lg bg-white border-bottom" data-aos="fade-down">
         <div className="container-fluid mx-5">
-          <h1>
+          <h1 data-aos="fade-right">
             <Link className="navbar-brand clr" to="/">Crimatch</Link>
           </h1>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
@@ -29,24 +30,17 @@ useEffect(() => {
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <div className="w-100">
-              <ul className="navbar-nav mb-2 mb-lg-0 mx-5 justify-content-center">
+              <ul className="navbar-nav mb-2 mb-lg-0 mx-5 justify-content-center" data-aos="fade-down">
                 <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
                 <li className="nav-item"><Link className="nav-link" to="/Listings">Listings</Link></li>
                 <li className="nav-item"><Link className="nav-link" to="/contact">Contact us</Link></li>
-                {/* Conditionally render Add New Listing link only if user is admin */}
-
-{/* 
-                {userData && userData.role === "admin" && ( */}
+                {userData && userData.role === "admin" && (
                   <li className="nav-item"><Link className="nav-link" to="/newlisting">Add New Listing</Link></li>
-{/*                   )}  */}
-                  
-
-
-
+                )}
               </ul>
             </div>
 
-            <div className="d-flex justify-content-end" style={{ width: "250px" }}>
+            <div className="d-flex justify-content-end" style={{ width: "250px" }} data-aos="fade-left">
               {token ? (
                 <button className="btn btn-outline-danger w-100 fs-5" onClick={handleLogout}>Logout</button>
               ) : (
@@ -60,31 +54,29 @@ useEffect(() => {
         </div>
       </nav>
 
-      <nav className="navbar navbar-light bg-light px-4">
-  <div className="container-fluid d-flex justify-content-between align-items-center w-100">
-    
+      {/* BOTTOM NAVBAR */}
+      <nav className="navbar navbar-light bg-light px-4" data-aos="fade-up">
+        <div className="container-fluid d-flex justify-content-between align-items-center w-100">
+          <div style={{ width: "150px" }}></div>
 
-    <div style={{ width: "150px" }}></div>
-    
+          <form className="d-flex" style={{ width: "400px" }} data-aos="zoom-in">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
 
-    <form className="d-flex" style={{ width: "400px" }}>
-      <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-      <button className="btn btn-outline-success" type="submit">Search</button>
-    </form>
-
-    <div>
-      <a href="/cart" className="btn btn-outline-secondary rounded-circle mx-2" style={{ height: "50px", width: "48px", border: "0" }}>
-        <ShoppingCartIcon />
-      </a>
-      <a href="/profile" className="btn btn-outline-secondary rounded-circle mx-2" style={{ height: "50px", width: "48px", border: "0" }}>
-        <PersonIcon />
-      </a>
-    </div>
-  </div>
-</nav>
-
+          <div data-aos="fade-left">
+            <a href="/cart" className="btn btn-outline-secondary rounded-circle mx-2" style={{ height: "50px", width: "48px", border: "0" }}>
+              <ShoppingCartIcon />
+            </a>
+            <a href="/profile" className="btn btn-outline-secondary rounded-circle mx-2" style={{ height: "50px", width: "48px", border: "0" }}>
+              <PersonIcon />
+            </a>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
+
 
 
