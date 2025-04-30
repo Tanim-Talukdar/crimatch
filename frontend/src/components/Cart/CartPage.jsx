e1a79c391daf86e98690cbc4d0d2b66fdbe1b362
Tanim-Talukdar <talukdertanim73@gmail.com>
2025-05-01 05:50:58 +0600

    Add files via upload

---
+++ frontend/src/components/Cart/CartPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CartProduct from './CartProduct'
+
+export default function CartPage() {
+  return (
+    <div className="container  m-auto mt-5" style={{width: "100vw"}}>
+        <CartProduct/>
+    </div>
+  )
+}
