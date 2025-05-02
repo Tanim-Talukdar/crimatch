bd1c2de2ea67b7a8772981fd02921d3e8f7538c1
Tanim-Talukdar <talukdertanim73@gmail.com>
2025-05-02 07:42:41 +0600

    Delete frontend/src/context/cart.jsx

--- frontend/src/context/cart.jsx
+++
@@ -1,6 +0,0 @@
-// src/api/cart.js
-import { client } from "../../client";
-
-export const getCartItems = () => client.get('/cart/user');
-
-export const removeFromCart = (id) => client.delete(`/cart/remove/${id}`);
